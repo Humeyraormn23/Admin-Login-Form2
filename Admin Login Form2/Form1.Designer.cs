c0e886097647a8de84a908f0d1e22c77e7091c54
humeyra.orman23@gmail.com <humeyra.orman23@gmail.com>
2024-09-02 19:46:59 +0300

    Proje dosyası ekle.

---
+++ Admin Login Form2/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Admin_Login_Form2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            checkBox1 = new CheckBox();
+            label1 = new Label();
+            label2 = new Label();
+            linkLabel1 = new LinkLabel();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(204, 162);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Giriş Yap";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(204, 197);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(109, 24);
+            checkBox1.TabIndex = 1;
+            checkBox1.Text = "Beni Hatırla";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(80, 94);
+            label1.Name = "label1";
+            label1.Size = new Size(118, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Kullanıcı Adınız :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(133, 128);
+            label2.Name = "label2";
+            label2.Size = new Size(65, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Şifreniz :";
+            label2.Click += label2_Click;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(321, 166);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(117, 20);
+            linkLabel1.TabIndex = 4;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Şifremi Unuttum";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(204, 87);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(234, 27);
+            textBox1.TabIndex = 5;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(204, 125);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(234, 27);
+            textBox2.TabIndex = 6;
+            textBox2.UseWaitCursor = true;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Candara", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.FromArgb(192, 0, 0);
+            label3.Location = new Point(190, 41);
+            label3.Name = "label3";
+            label3.Size = new Size(169, 24);
+            label3.TabIndex = 7;
+            label3.Text = "Admin Login Form";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            ClientSize = new Size(571, 291);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(linkLabel1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(checkBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private CheckBox checkBox1;
+        private Label label1;
+        private Label label2;
+        private LinkLabel linkLabel1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label3;
+    }
+}
